32d6b8ca167bef7fc8afbd73d8fd4a1f4157b0c9
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2024-04-18 02:47:24 +0530

    Add file ops required for commit log

--- LSMTree/commitlog/commitlog.go
+++ LSMTree/commitlog/commitlog.go
@@ -1,12 +1,69 @@
 package commitlog
 
 import (
-    "db/LSMTree/engine"
+	"bufio"
+	"log"
+	"os"
+
+	"db/LSMTree/engine"
 )
 
-type CommitLog interface {
+const FILE_PATH = "log.txt"
+
+type CommitLogInterface interface {
     append(entry engine.Record)
     readLog() []engine.Record
     size() uint64
     clear()
+}
+
+type CommitLog struct {
+    Filepath string 
+    Size     uint64
+}
+
+func (cl *CommitLog) initialiseCommitLog() {
+    cl.Filepath = FILE_PATH
+    cl.Size = 0
+}
+
+func (cl *CommitLog) NewCommitLog() {
+   cl.initialiseCommitLog()
+
+    _, err := os.Stat(cl.Filepath)
+    if !os.IsNotExist(err) {
+        log.Printf("Log exists, calculating size \n")
+        cl.Size = calculateLogSize(cl.Filepath)
+    } else {
+        log.Printf("Log doesn't exist, creating new file %s", cl.Filepath)
+        createLogFile(cl.Filepath)
+    }
+}
+
+func calculateLogSize(filepath string) uint64 {
+    file, err := os.Open(filepath)
+    if err != nil {
+        // TODO: This will panic and cause the entire engine to fail, errors should be handled by the engine
+        log.Panicf("Unable to open file %s", filepath)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    var size uint64 = 0
+
+    for scanner.Scan() {
+        size++
+    }
+    return size
+}
+
+func createLogFile(filepath string) {
+    file, err := os.Create(filepath)
+    
+    if err != nil {
+        // TODO: This will panic and cause the entire engine to fail, errors should be handled by the engine
+        log.Panicf("Unable to create file %s : %v", filepath, err)
+    }
+
+    defer file.Close()
 }
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"db/LSMTree/commitlog"
+	"fmt"
+)
 func main() {
     // TODO: Implement server for requests to the DB
+
+    cl := commitlog.CommitLog{}
+    cl.NewCommitLog()
+    fmt.Println(cl.Size)
 }
